310df196e842085ec014f94b190a4b85090f23d3
oliverfadhil <fadhilioliver1@gmail.com>
2024-01-23 12:15:30 +0300

    added a function that generates a grade from inputed marks in the studentgradegeneratorjs file and it didn't work

--- studentgradegenerator.js
+++ studentgradegenerator.js
@@ -1,1 +1,6 @@
-const gradegenerated=function Grade(){if(marks>=79){"A";if(marks<79>60){"B";if(marks>=49<=59){"c";if(marks>=40<=49){"D";if(marks<40){"E"}}}}}}                                                                  }
+function generatedGrade() {
+    let marks = document.getElementById("marks")
+    let marksValue = function Grade(){if(`${marks}`<=79){console.log("A");if(`${marks}`<79>60){"B";if(`${marks}`>=49<=59){"c";if(`${marks}`>=40<=49){"D";if(`${marks<40}`){"E"}}}}}}
+    let result = document.getElementById("result");result.innertext =`${marksValue}`;
+
+}
