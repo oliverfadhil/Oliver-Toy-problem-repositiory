019a1852e81e3f8e11a3f0ed2fadae894b261aa9
oliverfadhil <fadhilioliver1@gmail.com>
2024-01-23 17:01:32 +0300

    added event listener to form so as to now invoke result function using the input

--- studentgradegenerator.js
+++ studentgradegenerator.js
@@ -1,6 +1,23 @@
-function generatedGrade() {
-    let marks = document.getElementById("marks")
-    let marksValue = function Grade(){if(`${marks}`<=79){console.log("A");if(`${marks}`<79>60){"B";if(`${marks}`>=49<=59){"c";if(`${marks}`>=40<=49){"D";if(`${marks<40}`){"E"}}}}}}
-    let result = document.getElementById("result");result.innertext =`${marksValue}`;
+const grade = document.getElementById("marks");
+const form = document.getElementById("marks-form")
 
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const input = e.target.marks
+    result(input.value)
+    form.reset()
+   
+})
+
+function result(marks) {
+  if (marks >79) {
+    console.log("A");
+  } else if (marks>60 && marks<=79) {
+    console.log("B");
+  } else if (marks >49 && marks<= 59) {
+    console.log("C");
+  } else if (marks >= 40 && marks<= 49) {
+    console.log("D");
+  } else {console.log("E");
+  }
 }
