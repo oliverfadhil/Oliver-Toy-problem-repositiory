1a8cfe14a7bb66e1fbadd5cdcd28b844f97ec61d
oliverfadhil <fadhilioliver1@gmail.com>
2024-01-23 10:00:52 +0300

    updated the code of the function that generates the grade

--- studentgradegenerator.js
+++ studentgradegenerator.js
@@ -1,3 +1,1 @@
-const marks = parseInt(prompt("Enter student marks"))
-// toggles a window that prompts the user to input the marks that they would like to be calculated
-function grade(){if(marks>79){'A'}if (marks<79&&marks>60){'b'}if(marks<=59&&marks>49){'C'}if(marks<49&&marks>40){'D'}else if(marks<40){'E'}};
+const gradegenerated=function Grade(){if(marks>=79){"A";if(marks<79>60){"B";if(marks>=49<=59){"c";if(marks>=40<=49){"D";if(marks<40){"E"}}}}}}                                                                  }
